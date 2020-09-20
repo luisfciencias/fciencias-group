6ffd8766501a62456dd848570d1f775e9969f9e0
Pablo Galaviz <pgalaviz@cmri.org.au>
2020-09-20 20:19:56 +1000

    cambie el color de la grafica de rojo a verde

--- plot.py
+++ plot.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0.02, 10, 100)
-y = np.sin(x)/x
+def plot():
+    x = np.linspace(0.02, 10, 100)
+    y = np.sin(x)/x
 
-plt.plot(x,y, 'g-')
-plt.show()
+    plt.plot(x,y, 'g-')
+    plt.show()
+
+if __name__ == '__main__':
+    exit(plot())
