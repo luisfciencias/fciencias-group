80295210e882895bdcdafb91ae5b8f3622bf0454
Pablo Galaviz <pgalaviz@cmri.org.au>
2020-09-20 19:59:29 +1000

    para que se vea mas chido

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+
 def hello():
     print("hello Ali y amlove")
 
 
-hello()
+if __name__ == '__main__':
+    exit(hello())
+    return 0
--- plot.py
+++ plot.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 x = np.linspace(0.02, 10, 100)
 y = np.sin(x)/x
 
-plt.plot(x,y, 'r--')
+plt.plot(x,y, 'g-')
 plt.show()
