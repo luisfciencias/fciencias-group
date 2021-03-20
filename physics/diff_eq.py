4baa180d893ac9aabeaeeebdc49d813560571096
Luis Torres <luisfciencias@gmail.com>
2021-03-20 18:38:55 +1100

    FIG: Displays time integration

--- physics/diff_eq.py
+++ physics/diff_eq.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from scipy import integrate
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 plt.rcParams['mathtext.fontset'] = 'cm'
 
 
@@ -31,11 +33,23 @@ y0 = x0, p0
 t = np.linspace(0, 2 * np.pi, 500)
 
 # integrator for different parameters
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))  # type: Figure, list[Axes]
 for mu_param in mu_list:
     ret = integrate.odeint(dynamical_system, y0, t, args=(mu_param,))
     df_res = pd.DataFrame(ret, columns=['x', 'p'])
-    plt.plot(t, df_res['x'], label=r"$\mu = {}$".format(mu_param))
+    axes[0].plot(t, df_res['x'], label=r"$\mu = {}$".format(mu_param))
+    axes[1].plot(t, df_res['p'])
+    axes[2].plot(df_res['x'], df_res['p'])
     plt.xlabel("$t$", fontsize=12)
     plt.ylabel("$X$", fontsize=12)
-plt.legend(fontsize=12)
-plt.show()
+
+axes[0].legend(fontsize=12)
+[ax.set_xlabel(x_label, fontsize=12) for ax, x_label in zip(axes, ["$t$", "$t$", "$x$"])]
+[ax.set_ylabel(y_label, fontsize=12) for ax, y_label in zip(axes, ["$x$", "$p$", "$p$"])]
+
+axes[0].set_title(r"$\ddot{x} - \mu (1-x^2) \dot{x} + x = 0$")
+
+fig.tight_layout()
+output_figure = "fig_output.png"
+plt.savefig(output_figure)
+print("Output to: {}".format(output_figure))
